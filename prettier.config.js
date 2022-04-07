d0cba7e8c0cef4b14fffdeb8eb127777b1bc266c
Ryan <trbuck13@gmail.com>
2022-04-06 21:49:59 -0500

    add configs

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 2,
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    arrowParens: 'always',
+    plugins: [require('prettier-plugin-tailwindcss')],
+  };
+  
